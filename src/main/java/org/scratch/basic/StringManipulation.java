2f08b165cf9b58a43a2f665a66e878e04c3a616d
Khazri Mamedov <khazri.mamedov@gmail.com>
2021-01-21 13:38:58 +0300

    Added changed files

--- src/main/java/org/scratch/basic/StringManipulation.java
+++ src/main/java/org/scratch/basic/StringManipulation.java
@@ -1,5 +1,93 @@
 package org.scratch.basic;
 
-public class StringManipulation
-{
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class StringManipulation {
+    /**
+     * Counting duplicate characters old-fashion way. UTF-32 isn't supported (low, high surrogates)
+     */
+    public Map<Character, Long> duplicateCharsIterate(final String value) {
+        Map<Character, Long> result = new HashMap<>();
+        
+        for (char ch : value.toCharArray()) {
+            result.merge(ch, 1L, Long::sum);
+        }
+        
+        return result;
+    }
+    
+    /**
+     * Counting duplicate characters with Stream API. UTF-32 isn't supported (low, high surrogates)
+     */
+    public Map<Character, Long> duplicateCharsStream(final String value) {
+        return value.chars()
+                .mapToObj(intChar -> (char) intChar)
+                .collect(Collectors.groupingBy(ch -> ch, Collectors.counting()));
+    }
+    
+    /**
+     * First non repeated character from input value.
+     * IllegalArgumentException in case of all repeated characters
+     */
+    public char firstNonRepeated(final String value) {
+        LinkedHashMap<Character, Long> result = value.chars()
+                .mapToObj(ch -> (char) ch)
+                .collect(Collectors.groupingBy(ch -> ch, LinkedHashMap::new, Collectors.counting()));
+        
+        return result.entrySet().stream()
+                .filter(characterLongEntry -> characterLongEntry.getValue() == 1L)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new)
+                .getKey();
+    }
+    
+    /**
+     * Reverse letters in a word. Splitting sentence by whitespace
+     */
+    public String reverseLetters(final String sentence) {
+        StringBuilder result = new StringBuilder();
+        
+        final String[] words = sentence.split(" ");
+        for (String word : words) {
+            StringBuilder reverseWord = new StringBuilder(word).reverse();
+            result.append(reverseWord).append(" ");
+        }
+        
+        return result.toString().trim();
+    }
+    
+    /**
+     * Reverse letters in a word with Stream API. Splitting sentence by whitespace
+     */
+    public String reverseLettersStream(final String sentence) {
+        Pattern pattern = Pattern.compile(" +");
+        return pattern.splitAsStream(sentence)
+                .map(s -> new StringBuilder(s).reverse())
+                .collect(Collectors.joining(" "));
+    }
+    
+    /**
+     * Checking all characters are digits
+     */
+    public boolean isAllDigits(final String value) {
+        for(char ch : value.toCharArray()) {
+            if(!Character.isDigit(ch)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Checking all characters are digits with Stream API. allMatch will be slower than reverting anyMatch
+     */
+    public boolean isAllDigitsStream(final String value) {
+        return !value.chars()
+                .anyMatch(i -> !Character.isDigit(i));
+    }
 }
--- src/test/java/org/scratch/basic/StringManipulationTest.java
+++ src/test/java/org/scratch/basic/StringManipulationTest.java
@@ -1,5 +1,84 @@
 package org.scratch.basic;
 
-public class StringManipulationTest
-{
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class StringManipulationTest {
+    private final StringManipulation stringManipulation = new StringManipulation();
+    
+    @Test
+    public void testDuplicateCharsIterateSuccess() {
+        final String testValue = "abba";
+        Map<Character, Long> result = stringManipulation.duplicateCharsIterate(testValue);
+        
+        assertSame(result.get('a'), 2L);
+        assertSame(result.get('b'), 2L);
+    }
+    
+    @Test
+    public void testDuplicateCharsStreamSuccess() {
+        final String testValue = "abba";
+        Map<Character, Long> result = stringManipulation.duplicateCharsStream(testValue);
+        
+        assertSame(result.get('a'), 2L);
+        assertSame(result.get('b'), 2L);
+    }
+    
+    @Test
+    public void testFirstNonRepeatedSuccess() {
+        final String testValue = "accb";
+        char result = stringManipulation.firstNonRepeated(testValue);
+        
+        assertSame(result, 'a');
+    }
+    
+    @Test
+    public void testFirstNonRepeatedFail() {
+        final String testValue = "ccc";
+        assertThrows(IllegalArgumentException.class, () -> stringManipulation.firstNonRepeated(testValue));
+    }
+    
+    @Test
+    public void testReverseLettersSuccess() {
+        final String sentence = "Are you this?";
+        String result = stringManipulation.reverseLetters(sentence);
+        
+        assertEquals(result, "erA uoy ?siht");
+    }
+    
+    @Test
+    public void testReverseLettersStreamSuccess() {
+        final String sentence = "Are you this?";
+        String result = stringManipulation.reverseLettersStream(sentence);
+        
+        assertEquals(result, "erA uoy ?siht");
+    }
+    
+    @Test
+    public void testIsAllDigitsSuccess() {
+        final String testValue = "1122";
+        boolean result = stringManipulation.isAllDigits(testValue);
+        
+        assertTrue(result);
+    }
+    
+    @Test
+    public void testIsAllDigitsFail() {
+        final String testValue = "1122a";
+        boolean result = stringManipulation.isAllDigits(testValue);
+        
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testIsAllDigitsStreamSuccess() {
+        final String testValue = "1122";
+        boolean result = stringManipulation.isAllDigitsStream(testValue);
+        
+        assertTrue(result);
+    }
 }
